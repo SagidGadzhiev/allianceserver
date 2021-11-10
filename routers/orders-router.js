548c484f37d7c1aa041487a570809f200d4270e2
SagidGadzhiev <sagidkg36@gmail.com>
2021-11-10 17:12:31 +0600

    commit

--- routers/orders-router.js
+++ routers/orders-router.js
@@ -10,8 +10,8 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     const currentDate = new Date();
     const order = req.body;
-    const number = await orderModel.find().then(data => data[data.length - 1].orderNumber + 1);
-    const orderDate = await orderModel.find().then(data => data[data.length - 1].date = `${('0' + currentDate.getDate()).slice(-2)}-${('0' + (currentDate.getMonth() + 1)).slice(-2)}-${('0' + currentDate.getFullYear()).slice(-2)}`);
+    const number = await orderModel.find().then(data => data.length) === 0 ? 1 : await orderModel.find().then(data => data[data.length - 1].orderNumber + 1);
+    const orderDate = await orderModel.find().then(data => data.length) === 0 ? `${('0' + currentDate.getDate()).slice(-2)}-${('0' + (currentDate.getMonth() + 1)).slice(-2)}-${('0' + currentDate.getFullYear()).slice(-2)}` : await orderModel.find().then(data => data[data.length - 1].date = `${('0' + currentDate.getDate()).slice(-2)}-${('0' + (currentDate.getMonth() + 1)).slice(-2)}-${('0' + currentDate.getFullYear()).slice(-2)}`);
     await orderModel.create({...order, date: orderDate, orderNumber: number}, (err, doc) => {
         if (err)
             console.log(err);
