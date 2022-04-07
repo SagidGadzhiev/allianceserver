2a55413c4fc0c445aab7848dc842375d985dd4d7
SagidGadzhiev <sagidkg36@gmail.com>
2022-04-07 12:22:43 +0600

    new sale routers added

--- routers/saleProducts-router.js
+++ routers/saleProducts-router.js
@@ -4,7 +4,27 @@ const saleProds = require('../models/saleProducts-model');
 
 router.get('/', async (req, res) => {
     const it = await saleProds.find().then(data => data);
-    return res.json(it)
+    return res.json(it);
+});
+
+router.get('/:code', async (req, res) => {
+    const it = await saleProds.findOne({code: req.params.code}, null, {}, (err, doc) => {
+        if (err)
+            console.log(err);
+        console.log(doc);
+    });
+    return res.json(it);
+});
+
+router.get('/limit/:limit', async (req, res) => {
+    await saleProds.find({}, function (err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    })
+        .limit(+req.params.limit);
 });
 
 router.post('/', async (req, res) => {
@@ -12,18 +32,27 @@ router.post('/', async (req, res) => {
     await saleProds.create(saleProd, (err, doc) => {
         if (err)
             console.log(err);
-        console.log(doc)
+        console.log(doc);
     });
-    return res.json({status: 'success', saleProd})
+    return res.json({ status: 'success', saleProd });
 });
 
 router.delete('/', async (req, res) => {
     await saleProds.deleteMany({}, {}, (err, doc) => {
         if (err)
             console.log(err);
-        console.log(doc)
+        console.log(doc);
     });
-    return res.json({status: 'successfully deleted'})
+    return res.json({ status: 'successfully deleted' });
+});
+
+router.delete('/:code', async (req, res) => {
+    await saleProds.deleteOne({ code: req.params.code }, {}, (err, doc) => {
+        if (err)
+            console.log(err);
+        console.log(doc);
+    });
+    return res.json({ status: `product with id: ${req.params.code} successfully deleted` });
 });
 
 module.exports = router;
