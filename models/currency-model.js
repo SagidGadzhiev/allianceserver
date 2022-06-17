6d2bc350330f6d6139a94624303b26bdeff070eb
SagidGadzhiev <sagidkg36@gmail.com>
2022-06-17 17:34:10 +0600

    currency model added

--- models/currency-model.js
+++ models/currency-model.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const currencySchema = new Schema({
-    _id: {type: Number, required: true, unique: true},
-    currency: {type: Number}
+    currency: { type: Number }
 }, {
-    _id: false,
     versionKey: false
 });
 
